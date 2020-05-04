f18858ad4848d05f93cd9e1e5b8491c89ccaae04
Wesley Klop <wesley19097@gmail.com>
2020-05-04 21:33:03 +0200

    Move token banner location

--- resources/views/components/header-voter.blade.php
+++ resources/views/components/header-voter.blade.php
@@ -1,6 +1,5 @@
 <header class="header">
     <a href="{{ route('voter.index') }}" class="header-title">Vote System</a>
-    @include('components.token-banner')
     <nav class="header-nav">
         @auth('voter')
             <a href="{{ route('voter.logout') }}" class="hover:text-failure">Exit</a>
