a552db5084c1bd4560a12443bb356c53b408ccf9
Wesley Klop <wesley19097@gmail.com>
2020-07-07 17:34:39 +0200

    Force https scheme when app urls starts with https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (Str::startsWith(config('app.url'), 'https://')) {
+            URL::forceScheme('https');
+        }
     }
 }
