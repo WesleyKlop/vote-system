48cceef502b383a98d37f550d79caa618fe085b9
Wesley Klop <wesley19097@gmail.com>
2020-07-09 00:03:49 +0200

    Fix issue with cached configuration files breaking the management of our Admin user

--- config/vote-system.php
+++ config/vote-system.php
@@ -5,4 +5,6 @@ return [
      * Length of Voter tokens
      */
     'token_length' => env('VS_TOKEN_LENGTH', 16),
+    'admin_name' => env('VS_ADMIN_NAME', 'admin'),
+    'admin_password' => env('VS_ADMIN_PASSWORD', 'password'),
 ];
