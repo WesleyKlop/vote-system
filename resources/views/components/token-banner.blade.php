704cdda99ec92e0470578d09ef8a038509fb7d03
Wesley Klop <wesley19097@gmail.com>
2021-02-17 10:46:52 +0100

    Remove newline in banner

--- resources/views/components/token-banner.blade.php
+++ resources/views/components/token-banner.blade.php
@@ -1,3 +1,1 @@
-<div class="banner">
-    Your token: <span class="font-mono">{{ trim(chunk_split(Auth::user()->token, 4, ' ')) }}</span>
-</div>
+<div class="banner">Your token: <span class="font-mono">{{ trim(chunk_split(Auth::user()->token, 4, ' ')) }}</span></div>
