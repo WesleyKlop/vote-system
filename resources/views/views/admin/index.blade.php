535a27ef8348c568e11d46513b5b59f6c8be37c7
Wesley Klop <wesley19097@gmail.com>
2021-02-20 16:21:25 +0100

    Fix missing newline

--- resources/views/views/admin/index.blade.php
+++ resources/views/views/admin/index.blade.php
@@ -4,7 +4,7 @@
     <div class="grid grid-cols-1 lg:grid-cols-2 gap-8 mb-24">
         <h1 class="title col-span-1 lg:col-span-2 px-8 sm:px-0 text-primary">@lang('Dashboard')</h1>
         <div class="banner col-span-1 lg:col-span-2">{!! $page->getWelcomeMessage() !!}
-            @lang('Voter statistics', ['total' => $page->getTotalVoterCount(), 'used' => $page->getUsedVoterCount() ])
+            @lang('Voter statistics', ['total' => $page->getTotalVoterCount(), 'used' => $page->getUsedVoterCount() ])<br />
             @lang('These statistics are based on the used tokens.')
         </div>
 
