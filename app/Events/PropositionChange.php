695c4e102e2f4961a4c30d332a582a8ec99523e2
Wesley Klop <wesley19097@gmail.com>
2021-02-21 12:30:10 +0100

    Broadcast Proposition visibility changes

---
+++ app/Events/PropositionChange.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Proposition;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class PropositionChange implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function __construct(public ?Proposition $proposition)
+    {
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn(): Channel
+    {
+        return new PrivateChannel('propositions');
+    }
+}
